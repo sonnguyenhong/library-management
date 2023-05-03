aab4ff39bc232a366f12d3af460abd090c845759
sonnguyenhong <sonnguyenhong291@gmail.com>
2023-05-03 21:46:32 +0700

    feature: fix

--- library-management-frontend/src/App.js
+++ library-management-frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World, I'm Nguyen Hong Son!</p>
+        <p>Hello World, I'm Nguyen Hong Son Updated!</p>
       </header>
     </div>
   );
