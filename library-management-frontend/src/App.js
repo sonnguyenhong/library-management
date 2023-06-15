ce6e7c1855129161d9d3a1615fff496d819e08bc
UngLH <lehongung.work@gmail.com>
2023-06-15 14:40:17 +0700

    add home screen

--- library-management-frontend/src/App.js
+++ library-management-frontend/src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
 import Login from "./pages/Login"
+import Home from "./pages/home";
 function App() {
   return (
-    <Login/>
+    <Home/>
+    // <Login/>
+
   );
 }
 
--- library-management-frontend/src/components/Logo.js
+++ library-management-frontend/src/components/Logo.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import logo from '../assets/images/logo.png'
-import '../assets/css/logo.css'
+import React from "react";
+import logo from "../assets/images/logo.png";
+import "../assets/css/logo.css";
 
 function component1(props) {
-    return (
-        <div>
-            <div className='logo'>
-                <div className='img'><img src={logo} className='logo' alt="logo" /></div>
-                <div className='library'>
-                    <h2>Library</h2>
-                    <h1>Management</h1>
-                </div>
-                
-            </div><p>Welcome! Let’s get started</p>
+  return (
+    <div>
+      <div className="logo">
+        <div className="img">
+          <img src={logo} className="logo" alt="logo" />
         </div>
-
-    );
+        <div className="library">
+          <h2>Library</h2>
+          <h1>Management</h1>
+        </div>
+      </div>
+      <p>Welcome! Let’s get started</p>
+    </div>
+  );
 }
 
 export default component1;
